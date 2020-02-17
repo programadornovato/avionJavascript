db07360d109f7b2ac9cfbcd2ade86c016731b9fe
Programador Novato <info@programadornovato.com>
2020-02-17 16:19:35 -0600

    Programar un juego🕹️ en javascript Vender el avión presidencial [03.- Renderizar imagen con canvas.]
    Video:
    https://youtu.be/2qNtcTmo_44

--- js/juego.js
+++ js/juego.js
@@ -1,1 +1,14 @@
-alert("hola desde javascript");
+//alert("hola desde javascript");
+var mapa=document.getElementById('mapa');
+var anchoMapa=mapa.clientWidth;
+var altoMapa=mapa.clientHeight;
+
+var canvas=document.createElement('canvas');
+canvas.width=anchoMapa;
+canvas.height=altoMapa;
+
+window.onload=function (event) {
+    canvas.getContext('2d').drawImage(mapa,0,0,anchoMapa,this.altoMapa);
+    document.body.appendChild(canvas);
+    canvas.setAttribute('id','canvas');
+}
