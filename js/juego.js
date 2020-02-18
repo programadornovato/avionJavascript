ac3918cf40dde94c7e758ff2eec6b51d098945f1
Programador Novato <info@programadornovato.com>
2020-02-18 05:50:38 -0600

    Programar un juego🕹️en javascript Vender el avión presidencial [06.- Rellenar completamente un país]
    Video:
    https://youtu.be/2zi1rg21rwo

--- js/juego.js
+++ js/juego.js
@@ -7,6 +7,9 @@ var canvas = document.createElement('canvas');
 canvas.width = anchoMapa;
 canvas.height = altoMapa;
 
+var matriz=[];
+var contador=0;
+var verPais=true;
 window.onload = function (event) {
     canvas.getContext('2d').drawImage(mapa, 0, 0, anchoMapa, this.altoMapa);
     document.body.appendChild(canvas);
@@ -17,9 +20,11 @@ document.addEventListener('mousemove', function (event) {
     document.getElementById('salida').innerHTML = `ROJO=${datosDelPixel[0]} VERDE=${datosDelPixel[1]} AZUL=${datosDelPixel[2]} ALFA=${datosDelPixel[3]} `
 });
 document.addEventListener('click', function (event) {
-    llenarArea(event.offsetX, event.offsetY);
+    llenarArea(event.offsetX, event.offsetY,1);
+    llenarArea(event.offsetX, event.offsetY,-1);
+    llenarTodo();
 });
-function llenarArea(x, y) {
+function llenarArea(x, y,haciaArribaAbajo) {
     /*
     var ctx=canvas.getContext('2d');
     ctx.fillStyle="rgb(255, 0, 0)";
@@ -33,14 +38,32 @@ function llenarArea(x, y) {
     while (datosDelPixel[0] == 255 && datosDelPixel[1] == 255 && datosDelPixel[2] == 255) {
         while (datosDelPixel[0] == 255 && datosDelPixel[1] == 255 && datosDelPixel[2] == 255) {
             datosDelPixel = canvas.getContext('2d').getImageData(posx, posy, 1, 1).data;
+            /*
             var ctx = canvas.getContext('2d');
             ctx.fillStyle = "rgb(255, 0, 0)";
             ctx.fillRect(posx, posy, 1, 1);
+            */
             posx = posx + haciaDerechaIzquierda;
+            matriz[contador]={posx,posy};
+            contador++;
         }
-        posy=posy+1;
+        posy=posy+haciaArribaAbajo;
         haciaDerechaIzquierda=haciaDerechaIzquierda*-1;
-        posx=posx+(perdida*haciaDerechaIzquierda);
+        //posx=posx+(perdida*haciaDerechaIzquierda);
+        for (let i = 0; i < perdida; i++) {
+            posx=posx+haciaDerechaIzquierda;
+            matriz[contador]={posx,posy};
+            contador++;
+        }
         datosDelPixel = canvas.getContext('2d').getImageData(posx, posy, 1, 1).data;
+    }
+}
+function llenarTodo() {
+    for (let i = 0; i < contador; i++) {
+        if(verPais==true){
+            var ctx = canvas.getContext('2d');
+            ctx.fillStyle = "rgb(0, 0, 204)";
+            ctx.fillRect(matriz[i]['posx'],matriz[i]['posy'],1,1);
+        }
     }
 }
