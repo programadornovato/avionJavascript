bacfffdb2c836a42c520fb3aebd2783701c77901
Programador Novato <info@programadornovato.com>
2020-02-18 03:10:26 -0600

    Programar un juego🕹️ en javascript Vender el avión presidencial [05.- Detectar países]
    Video:
    https://youtu.be/FwqJJLNawdY

--- js/juego.js
+++ js/juego.js
@@ -1,28 +1,46 @@
 //alert("hola desde javascript");
-var mapa=document.getElementById('mapa');
-var anchoMapa=mapa.clientWidth;
-var altoMapa=mapa.clientHeight;
+var mapa = document.getElementById('mapa');
+var anchoMapa = mapa.clientWidth;
+var altoMapa = mapa.clientHeight;
 
-var canvas=document.createElement('canvas');
-canvas.width=anchoMapa;
-canvas.height=altoMapa;
+var canvas = document.createElement('canvas');
+canvas.width = anchoMapa;
+canvas.height = altoMapa;
 
-window.onload=function (event) {
-    canvas.getContext('2d').drawImage(mapa,0,0,anchoMapa,this.altoMapa);
+window.onload = function (event) {
+    canvas.getContext('2d').drawImage(mapa, 0, 0, anchoMapa, this.altoMapa);
     document.body.appendChild(canvas);
-    canvas.setAttribute('id','canvas');
+    canvas.setAttribute('id', 'canvas');
 }
-document.addEventListener('mousemove',function (event) {
-    var datosDelPixel=canvas.getContext('2d').getImageData(event.offsetX,event.offsetY,1,1).data;
-    document.getElementById('salida').innerHTML=`ROJO=${datosDelPixel[0]} VERDE=${datosDelPixel[1]} AZUL=${datosDelPixel[2]} ALFA=${datosDelPixel[3]} `
+document.addEventListener('mousemove', function (event) {
+    var datosDelPixel = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
+    document.getElementById('salida').innerHTML = `ROJO=${datosDelPixel[0]} VERDE=${datosDelPixel[1]} AZUL=${datosDelPixel[2]} ALFA=${datosDelPixel[3]} `
 });
-document.addEventListener('click',function (event) {
-    llenarArea(event.offsetX,event.offsetY);
+document.addEventListener('click', function (event) {
+    llenarArea(event.offsetX, event.offsetY);
 });
-function llenarArea(x,y){
-    var posx=x;
-    var posy=y;
+function llenarArea(x, y) {
+    /*
     var ctx=canvas.getContext('2d');
     ctx.fillStyle="rgb(255, 0, 0)";
     ctx.fillRect(posx,posy,6,6);
+    */
+    var posx = x;
+    var posy = y;
+    var datosDelPixel = canvas.getContext('2d').getImageData(posx, posy, 1, 1).data;
+    var haciaDerechaIzquierda=1;
+    var perdida=15;
+    while (datosDelPixel[0] == 255 && datosDelPixel[1] == 255 && datosDelPixel[2] == 255) {
+        while (datosDelPixel[0] == 255 && datosDelPixel[1] == 255 && datosDelPixel[2] == 255) {
+            datosDelPixel = canvas.getContext('2d').getImageData(posx, posy, 1, 1).data;
+            var ctx = canvas.getContext('2d');
+            ctx.fillStyle = "rgb(255, 0, 0)";
+            ctx.fillRect(posx, posy, 1, 1);
+            posx = posx + haciaDerechaIzquierda;
+        }
+        posy=posy+1;
+        haciaDerechaIzquierda=haciaDerechaIzquierda*-1;
+        posx=posx+(perdida*haciaDerechaIzquierda);
+        datosDelPixel = canvas.getContext('2d').getImageData(posx, posy, 1, 1).data;
+    }
 }
